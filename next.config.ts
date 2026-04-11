774c501afe522ee7c35ba7c9a9f53069dc221cbe
M. Iqbal Abdul Wakil <markopolo2212@gmail.comp>
2026-04-11 16:17:20 +0700

    add features

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   serverExternalPackages: [],
+  productionBrowserSourceMaps: false,
   experimental: {
+    serverMinification: true,
     optimizePackageImports: ['lucide-react'],
     serverActions: {
       bodySizeLimit: '5mb',
